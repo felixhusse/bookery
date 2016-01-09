fbd733e587a05d6b443d4f164ced5df86ca7463c
felixhusse <felix.husse@gmail.com>
2016-01-09 22:26:17 +0100

    - updated schema

--- book-importer/src/main/java/de/fatalix/book/importer/BookMigrator.java
+++ book-importer/src/main/java/de/fatalix/book/importer/BookMigrator.java
@@ -16,6 +16,8 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.solr.client.solrj.SolrServer;
@@ -31,6 +33,8 @@ import org.joda.time.DateTimeZone;
  */
 public class BookMigrator {
 
+    private static final Pattern PATTERN = Pattern.compile("[%\\.\"\\*/:<>\\?\\\\\\|\\+,\\.;=\\[\\]]");
+
     public static void clearDB(String solrURL, String solrCore) throws SolrServerException, IOException {
         SolrServer server = SolrHandler.createConnection(solrURL, solrCore);
         System.out.println("RESET:");
@@ -41,14 +45,14 @@ public class BookMigrator {
     public static List<File> findAllBooks(String importPath) throws IOException {
         List<File> result = new ArrayList<>();
         File importFolder = new File(importPath);
-        if(!importFolder.isDirectory()) {
+        if (!importFolder.isDirectory()) {
             throw new IOException(importFolder.getAbsolutePath() + " is not a folder!");
         }
         return walkTree(result, importFolder);
     }
 
     private static List<File> walkTree(List<File> result, File currentFolder) {
-        if(hasFolderBook(currentFolder)) {
+        if (hasFolderBook(currentFolder)) {
             result.add(currentFolder);
         } else {
             File[] subFolders = currentFolder.listFiles(new FileFilter() {
@@ -61,7 +65,7 @@ public class BookMigrator {
             for (File subFolder : subFolders) {
                 result = walkTree(result, subFolder);
             }
-            
+
         }
         return result;
     }
@@ -74,6 +78,57 @@ public class BookMigrator {
                 return file.getName().contains(".epub") || file.getName().contains(".mobi");
             }
         }).length > 0;
+    }
+
+    public static void filterBooks(List<File> bookFolders, File filteredFolder) throws IOException {
+        int total = bookFolders.size();
+        int counter = 0;
+        int filteredCounter = 0;
+        int percentageDone = 0;
+        Gson gson = new Gson();
+        File coverFilterFolder = new File(filteredFolder, "cover");
+        File descriptionFilterFolder = new File(filteredFolder, "description");
+        for (File bookFolder : bookFolders) {
+            BookEntry bookEntry = importBatchWise(bookFolder, gson);
+            boolean filtered = false;
+            if (bookEntry.getCover() == null) {
+                filteredCounter++;
+                try {
+                    String validFolderName =  toValidFileName(bookEntry.getAuthor() + "-" + bookEntry.getTitle());
+                    FileUtils.moveDirectory(bookFolder, new File(coverFilterFolder,validFolderName));
+                } catch (IOException ex) {
+                    System.out.println("Catched...");
+                }
+                System.out.println("Filtered " + filteredCounter + " of " + total);
+                filtered = true;
+            }
+
+            if (!filtered && bookEntry.getDescription() == null) {
+                filteredCounter++;
+                try {
+                    String validFolderName =  toValidFileName(bookEntry.getAuthor() + "-" + bookEntry.getTitle());
+                    FileUtils.moveDirectory(bookFolder, new File(descriptionFilterFolder,validFolderName));
+                } catch (IOException ex) {
+                    System.out.println("Catched...");
+                }
+                System.out.println("Filtered " + filteredCounter + " of " + total);
+                filtered = true;
+            }
+
+            counter++;
+
+            int currentProgress = counter * 100 / total;
+            if (currentProgress > percentageDone) {
+                percentageDone = currentProgress;
+                System.out.println(percentageDone + "% done..");
+            }
+
+        }
+        System.out.println("Finished processing");
+    }
+
+    private static String toValidFileName(String input) {
+        return input.replaceAll("[:\\\\/*\"?|<>']", " ");
     }
 
     /**
@@ -89,7 +144,7 @@ public class BookMigrator {
     public static void importBooks(String solrURL, String solrCore, int batchSize, List<File> bookFolders, boolean reset) throws IOException, SolrServerException {
 
         SolrServer server = SolrHandler.createConnection(solrURL, solrCore);
-        if(reset) {
+        if (reset) {
             System.out.println("RESET:");
             server.deleteByQuery("*:*");
             server.commit();
@@ -103,21 +158,21 @@ public class BookMigrator {
         int total = bookFolders.size();
         int counter = 0;
         List<BookEntry> bookEntries = new ArrayList<>();
-        for(File bookFolder : bookFolders) {
+        for (File bookFolder : bookFolders) {
             bookEntries.add(importBatchWise(bookFolder, gson));
             counter++;
-            if(bookEntries.size() >= batchSize) {
+            if (bookEntries.size() >= batchSize) {
                 UpdateResponse response = SolrHandler.addBeans(server, bookEntries);
-                if(response.getStatus() != 0) {
+                if (response.getStatus() != 0) {
                     throw new SolrServerException("Update failed with CODE " + response.getStatus());
                 }
                 bookEntries.clear();
                 System.out.println("Processed " + counter + " of " + total);
             }
         }
-        if(bookEntries.size() > 0) {
+        if (bookEntries.size() > 0) {
             UpdateResponse response = SolrHandler.addBeans(server, bookEntries);
-            if(response.getStatus() != 0) {
+            if (response.getStatus() != 0) {
                 throw new SolrServerException("Update failed with CODE " + response.getStatus());
             }
             bookEntries.clear();
@@ -135,7 +190,7 @@ public class BookMigrator {
      */
     public static void exportBooks(String solrURL, String solrCore, int batchSize, String exportPath) throws SolrServerException, IOException {
         File exportFolder = new File(exportPath);
-        if(!exportFolder.isDirectory()) {
+        if (!exportFolder.isDirectory()) {
             throw new IOException(exportFolder.getAbsolutePath() + " is not a folder!");
         }
 
@@ -149,22 +204,22 @@ public class BookMigrator {
     private static BookEntry importBatchWise(File bookFolder, Gson gson) throws IOException {
         BookEntry bookEntry = new BookEntry();
 
-        for(File file : bookFolder.listFiles()) {
-            if(file.getName().contains(".mobi")) {
+        for (File file : bookFolder.listFiles()) {
+            if (file.getName().contains(".mobi")) {
                 byte[] bookData = Files.readAllBytes(file.toPath());
                 bookEntry.setMobi(bookData);
-            } else if(file.getName().contains(".jpg")) {
+            } else if (file.getName().contains(".jpg")) {
                 byte[] coverData = Files.readAllBytes(file.toPath());
                 bookEntry.setCover(coverData);
-            } else if(file.getName().contains(".epub")) {
+            } else if (file.getName().contains(".epub")) {
                 byte[] bookData = Files.readAllBytes(file.toPath());
                 bookEntry.setEpub(bookData);
-            } else if(file.getName().contains(".json")) {
+            } else if (file.getName().contains(".json")) {
                 BookMetaData bmd = gson.fromJson(IOUtils.toString(new FileInputStream(file), Charset.defaultCharset()), BookMetaData.class);
                 bookEntry.setAuthor(bmd.getAuthor()).setTitle(bmd.getTitle()).setIsbn(bmd.getIsbn())
                         .setPublisher(bmd.getPublisher()).setDescription(bmd.getDescription()).setLanguage(bmd.getLanguage())
                         .setMimeType(bmd.getMimeType()).setUploadDate(bmd.getUploadDate()).setReleaseDate(bmd.getReleaseDate());
-            } else if(file.getName().contains(".opf")) {
+            } else if (file.getName().contains(".opf")) {
                 bookEntry = parseOPF(file, bookEntry);
 
                 bookEntry.setMimeType("mobi").setUploadDate(new DateTime(DateTimeZone.UTC).toDate());
@@ -179,17 +234,17 @@ public class BookMigrator {
         QueryResponse response = SolrHandler.searchSolrIndex(server, "*:*", batchSize, offset);
         List<BookEntry> bookEntries = response.getBeans(BookEntry.class);
         System.out.println("Retrieved " + (bookEntries.size() + offset) + " of " + response.getResults().getNumFound());
-        for(BookEntry bookEntry : bookEntries) {
+        for (BookEntry bookEntry : bookEntries) {
             String bookTitle = bookEntry.getTitle();
             bookTitle = bookTitle.replaceAll(":", " ");
             File bookFolder = new File(exportFolder, bookEntry.getAuthor() + "-" + bookTitle);
             bookFolder.mkdirs();
-            if(bookEntry.getCover() != null) {
+            if (bookEntry.getCover() != null) {
                 if (bookEntry.getEpub() != null) {
                     File bookData = new File(bookFolder, bookEntry.getAuthor() + "-" + bookTitle + ".epub");
                     Files.write(bookData.toPath(), bookEntry.getMobi(), StandardOpenOption.CREATE_NEW);
                 }
-                if (bookEntry.getMobi()!=null) {
+                if (bookEntry.getMobi() != null) {
                     File bookData = new File(bookFolder, bookEntry.getAuthor() + "-" + bookTitle + ".mobi");
                     Files.write(bookData.toPath(), bookEntry.getMobi(), StandardOpenOption.CREATE_NEW);
                 }
@@ -198,16 +253,16 @@ public class BookMigrator {
 
                 File metaDataFile = new File(bookFolder, bookEntry.getAuthor() + "-" + bookTitle + ".json");
                 BookMetaData metaData = new BookMetaData(bookEntry.getAuthor(), bookEntry.getTitle(), bookEntry.getIsbn(),
-                                                         bookEntry.getPublisher(), bookEntry.getDescription(), bookEntry.getLanguage(),
-                                                         bookEntry.getReleaseDate(), bookEntry.getMimeType(), bookEntry.getUploadDate(),
-                                                         bookEntry.getViewed(),bookEntry.getShared());
+                        bookEntry.getPublisher(), bookEntry.getDescription(), bookEntry.getLanguage(),
+                        bookEntry.getReleaseDate(), bookEntry.getMimeType(), bookEntry.getUploadDate(),
+                        bookEntry.getViewed(), bookEntry.getShared());
                 gson.toJson(metaData);
                 Files.write(metaDataFile.toPath(), gson.toJson(metaData).getBytes(), StandardOpenOption.CREATE_NEW);
             }
 
         }
 
-        if(response.getResults().getNumFound() > offset) {
+        if (response.getResults().getNumFound() > offset) {
             exportBatchWise(server, exportFolder, batchSize, offset + batchSize, gson);
         }
 
@@ -217,10 +272,10 @@ public class BookMigrator {
         List<String> lines = Files.readAllLines(pathToOPF.toPath(), Charset.forName("UTF-8"));
         boolean multiLineDescription = false;
         String description = "";
-        for(String line : lines) {
-            if(multiLineDescription) {
+        for (String line : lines) {
+            if (multiLineDescription) {
                 multiLineDescription = false;
-                if(line.split("<").length == 1) {
+                if (line.split("<").length == 1) {
                     multiLineDescription = true;
                     description = description + line;
                 } else {
@@ -228,15 +283,15 @@ public class BookMigrator {
                     description = StringEscapeUtils.unescapeXml(description);
                     bmd.setDescription(description);
                 }
-            } else if(line.contains("dc:title")) {
+            } else if (line.contains("dc:title")) {
                 String title = line.split(">")[1].split("<")[0];
                 bmd.setTitle(title);
-            } else if(line.contains("dc:creator")) {
+            } else if (line.contains("dc:creator")) {
                 String creator = line.split(">")[1].split("<")[0];
                 bmd.setAuthor(creator);
-            } else if(line.contains("dc:description")) {
+            } else if (line.contains("dc:description")) {
                 String value = line.split(">")[1];
-                if(value.split("<").length == 1) {
+                if (value.split("<").length == 1) {
                     multiLineDescription = true;
                     description = value;
                 } else {
@@ -244,19 +299,19 @@ public class BookMigrator {
                     value = StringEscapeUtils.unescapeXml(value);
                     bmd.setDescription(value);
                 }
-            } else if(line.contains("dc:publisher")) {
+            } else if (line.contains("dc:publisher")) {
                 String value = line.split(">")[1].split("<")[0];
                 bmd.setPublisher(value);
-            } else if(line.contains("dc:date")) {
+            } else if (line.contains("dc:date")) {
                 String value = line.split(">")[1].split("<")[0];
                 DateTime dtReleaseDate = new DateTime(value, DateTimeZone.UTC);
-                if(dtReleaseDate.getYear() != 101) {
+                if (dtReleaseDate.getYear() != 101) {
                     bmd.setReleaseDate(dtReleaseDate.toDate());
                 }
-            } else if(line.contains("dc:language")) {
+            } else if (line.contains("dc:language")) {
                 String value = line.split(">")[1].split("<")[0];
                 bmd.setLanguage(value);
-            } else if(line.contains("opf:scheme=\"ISBN\"")) {
+            } else if (line.contains("opf:scheme=\"ISBN\"")) {
                 String value = line.split(">")[1].split("<")[0];
                 bmd.setIsbn(value);
             }
